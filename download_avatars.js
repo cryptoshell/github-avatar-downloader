dbc55f41f9653db2658c7946d13a2af7a850f342
Yelim Oh <yelim.oh1@gmail.com>
2017-07-04 00:26:09 +0000

    Finished program to log avatarUrl of all contributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,35 @@
-var request = require("request");
+const request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function requestOptions(path) {
+  return {
+    url: 'https://api.github.com' + path,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader Project'
+    },
+    qs: {
+      access_token: process.env.GITHUB_ACCESS_TOKEN
+    }
+  };
+}
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  const path = '/repos/'+repoOwner+'/'+repoName+'/contributors';
+  request(requestOptions(path), function (error, response, body) {
+    try {
+      const data = JSON.parse(body);
+      cb(data);
+    } catch (err) {
+      console.log('Failed to parse body content');
+    }
+  });
+}
+
+
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
+
+getRepoContributors(repoOwner, repoName, (data) => {
+  data.forEach((contributor) => {
+    console.log(contributor.avatar_url);
+  })
+});
